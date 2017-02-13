5bde7aacde588fb87d37a7ca4e548b3dac003ab1
Collin Rea <collinr01@gmail.com>
2017-02-13 09:17:01 -0600

    Add topic index route

--- web/controllers/topic_controller.ex
+++ web/controllers/topic_controller.ex
@@ -3,12 +3,24 @@ defmodule Discuss.TopicController do
 
   alias Discuss.Topic
 
+  def index(conn, _params) do
+    topics = Repo.all(Topic)
+    render conn, "index.html", topics: topics
+  end
+
   def new(conn, _params) do
     changeset = Topic.changeset(%Topic{}, %{})
     render conn, "new.html", changeset: changeset
   end
 
   def create(conn, %{"topic" => topic}) do
-    IO.inspect(topic)
+    changeset = Topic.changeset(%Topic{}, topic)
+
+    case Repo.insert(changeset) do
+      {:ok, post} -> 
+        render conn, "index.html"
+      {:error, changeset} -> 
+        render conn, "new.html", changeset: changeset
+    end
   end
 end
