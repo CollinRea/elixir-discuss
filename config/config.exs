fe83fb1adb571306467d31b12900129f6f3413d5
Collin Rea <collinr01@gmail.com>
2017-02-15 07:37:21 -0600

    Add Ueberauth dependency and config to use github for authentication

--- config/config.exs
+++ config/config.exs
@@ -25,3 +25,13 @@ config :logger, :console,
 # Import environment specific config. This must remain at the bottom
 # of this file so it overrides the configuration defined above.
 import_config "#{Mix.env}.exs"
+
+config :ueberauth, Ueberauth,
+  providers: [
+    github: { Ueberauth.Strategy.Github, [] }
+  ]
+
+config :ueberauth, Ueberauth.Strategy.Github.OAuth,
+  client_id: "24463d152342939df8f2",
+  client_secret: "386b098c033934dc4e8d08536826bcc52db35229"
+  
